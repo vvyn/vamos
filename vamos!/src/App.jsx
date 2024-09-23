c8232bbf7d8d3c8f19a667c174f12edbb4826efd
Riya Gandhi <griya2001@gmail.com>
2024-09-23 16:45:55 -0500

    Added the chatbot functionality -Riya

--- vamos!/src/App.jsx
+++ vamos!/src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {Home} from './Home/Home';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen w-screen bg-gradient-to-br from-[#2C3E50] to-[#BDC3C7]"> 
+        <Router>
+            <Routes>
+                <Route exact path = "/" element = {<Home />}>/</Route>
+            </Routes>
+          </Router>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
