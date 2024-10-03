d0661f11d24a264e58150d62e7b8ba02fe65bab7
Riya Gandhi <griya2001@gmail.com>
2024-10-03 15:50:46 -0500

    Added cards mastered

--- vamos!/src/Home/ProgressTracker.jsx
+++ vamos!/src/Home/ProgressTracker.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect } from 'react';
 
-export function ProgressTracker() {
+export function ProgressTracker({masteredCount}) {
 
   return (
     <div className="bg-[#FAFCFC] w-full rounded-[34px] overflow-hidden shadow-lg flex flex-col ">
@@ -13,8 +13,9 @@ export function ProgressTracker() {
             <div className="bg-gradient-to-br from-[#2C3E50] to-[#BDC3C7] w-8 h-8 rounded-full"></div>
         </div>
       </div>
-      <div className="overflow-y-auto p-4 bg-[#AEB2B5] h-44">
-        5/100 Cards Mastered
+      <div className="overflow-y-auto p-4 bg-[#AEB2B5] h-44 flex items-center justify-center">
+                <span className="text-black font-bold text-4xl mr-2">{masteredCount}/100</span>
+                <span className="text-black font-semibold text-4xl">Cards Mastered</span>
       </div>
     </div>
   );
